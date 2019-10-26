559a464284df4e6777db14958e5d9b1bbf58b4d5
Ravil <graf.rav@gmail.com>
2019-10-26 13:14:49 +0300

    project fix

--- UDPGameServer/Program.cs
+++ UDPGameServer/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace UDPGameServer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
+        
+        
     }
 }
