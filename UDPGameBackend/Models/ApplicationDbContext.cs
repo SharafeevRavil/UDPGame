5f3066140446b4333ce3b579e463168db17b05d5
Ravil <graf.rav@gmail.com>
2019-12-21 08:12:01 +0300

    wip

---
+++ UDPGameBackend/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UDPGameBackend.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Characteristic> CharacteristicsUsers { get; set; }
+        public DbSet<CharacteristicUser> CharacteristicUsers { get; set; }
+    }
+}
---
+++ UDPGameBackend/Models/Characteristic.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace UDPGameBackend.Models
+{
+    public class Characteristic
+    {
+        public int CharacteristicId { get; set; }
+        public int Name { get; set; }
+        
+        public List<CharacteristicUser> CharacteristicUsers { get; set; }
+    }
+}
